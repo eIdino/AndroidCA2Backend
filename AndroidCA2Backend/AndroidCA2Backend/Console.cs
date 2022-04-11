8de5d67f136310bac4936c6505f64a001d8ecaeb
eIdino <x00153243@mytudublin.ie>
2022-04-11 16:43:07 +0100

    Added console class

---
+++ AndroidCA2Backend/AndroidCA2Backend/Console.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AndroidCA2Backend
+{
+    public class Console
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [JsonIgnore]
+        public string ConsoleInfo
+        {
+            get
+            {
+                return $"Console: {Name}";
+            }
+        }
+    }
+}
